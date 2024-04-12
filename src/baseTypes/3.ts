0cd9eedee8d3cb43375f5a3bc88a33e83c319cca
Granovskiy Uriy <silaveri777@gmail.net>
2024-04-12 13:40:09 +0300

    6

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,11 @@
-let some:unknown;
+let some: unknown;
 some = 'Text';
+
 let str: string;
-str = some;
+
+if (typeof some === 'string' || some instanceof String) {
+    str = some as string; // Тут ми використовуємо уточнення типів (as string)
+}
+
 
 export {};
